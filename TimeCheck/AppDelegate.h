5db84d1ea22559229a416e7990021896333f541b
tonosama <president.12kingdoms@gmail.com>
2014-10-12 22:29:35 +0900

    Initial Commit

---
+++ TimeCheck/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimeCheck
+//
+//  Created by 寺本生 on 2014/10/12.
+//  Copyright (c) 2014年 tono. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
