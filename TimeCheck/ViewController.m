4cb899cb062ac2dd8a869e67489362015a4ea689
tonosama <president.12kingdoms@gmail.com>
2014-10-18 11:43:35 +0900

    ・変数名は頭は小文字にしました ・@property (nonatomic) IBOutlet UITextFieldに修正しました ・NSIntegerを追加しました。

--- TimeCheck/ViewController.m
+++ TimeCheck/ViewController.m
@@ -5,19 +5,23 @@
 //  Created by tonosama on 2014/10/12.
 //  Copyright (c) 2014年 tono. All rights reserved.
 //
+//- 範囲指定は、開始時刻を含み、終了時刻は含まないと判断すること。
+//- ただし開始時刻と終了時刻が同じ場合は含むと判断すること。
+//- 開始時刻が22時で終了時刻が5時、というような指定をされても動作すること。
+//- 可読性、保守性、拡張性を考慮すること。
 
 #import "ViewController.h"
 
-@interface ViewController ()
-// 開始時刻
-@property (strong, nonatomic) IBOutlet UITextField *StartTimeText;
-// 終了時刻
-@property (strong, nonatomic) IBOutlet UITextField *EndTimeText;
-// 指定時刻
-@property (strong, nonatomic) IBOutlet UITextField *DesignationTimeText;
+@interface ViewController (){
+    NSInteger _startTime;
+    NSInteger _endTime;
+    NSInteger _designationTime;
+}
 
-// 判定ボタン
-@property (strong, nonatomic) IBOutlet UIButton *TimeCheckBtn;
+@property (nonatomic) IBOutlet UITextField *startTimeText; // 開始時刻
+@property (nonatomic) IBOutlet UITextField *endTimeText; // 終了時刻
+@property (nonatomic) IBOutlet UITextField *designationTimeText; // 指定時刻
+@property (nonatomic) IBOutlet UIButton *timeCheckBtn; // 判定ボタン
 
 - (IBAction)TimeCheckAct:(id)sender;
 - (Boolean)isCheck;
@@ -31,15 +35,11 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-//- 範囲指定は、開始時刻を含み、終了時刻は含まないと判断すること。
-//- ただし開始時刻と終了時刻が同じ場合は含むと判断すること。
-//- 開始時刻が22時で終了時刻が5時、というような指定をされても動作すること。
-//- 可読性、保守性、拡張性を考慮すること。
 
 
-
+// 判定ボタンのアクション
 - (IBAction)TimeCheckAct:(id)sender {
-    
+
     if([self isTime] == YES ){
         [self isCheck];
         
@@ -50,29 +50,30 @@
 - (Boolean)isCheck{
 
     UIAlertController *alertError = [UIAlertController alertControllerWithTitle:@"範囲判定" message:@"範囲外です" preferredStyle:UIAlertControllerStyleAlert];
-
+    _startTime = _startTimeText.text.integerValue;
+    _endTime = _endTimeText.text.integerValue;
+    _designationTime = _designationTimeText.text.integerValue;
+    
     // ex. 開始時刻：23時 終了時刻：1時 指定時刻：24時
-    if (_StartTimeText.text.integerValue > _EndTimeText.text.integerValue) {
-        if (_EndTimeText.text.integerValue <= _DesignationTimeText.text.integerValue  &&
-            _DesignationTimeText.text.integerValue < _StartTimeText.text.integerValue) {
+    if (_startTime > _endTime) {
+        if (_endTimeText.text.integerValue <= _designationTimeText.text.integerValue  &&
+            _designationTime < _startTime) {
 
             [alertError addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
             [self presentViewController:alertError animated:YES completion:nil];
             
             return NO;
         }
-    }else if(_StartTimeText.text.integerValue < _EndTimeText.text.integerValue){
-        if (_EndTimeText.text.integerValue <= _DesignationTimeText.text.integerValue  ||
-            _DesignationTimeText.text.integerValue <
-            _StartTimeText.text.integerValue) {
+    }else if(_startTime < _endTime){
+        if (_endTime <= _designationTime  || _designationTime <_startTime) {
 
             [alertError addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
             [self presentViewController:alertError animated:YES completion:nil];
 
             return NO;
         }
-    }else if (_StartTimeText.text.integerValue == _EndTimeText.text.integerValue){
-        if (_StartTimeText.text.integerValue != _DesignationTimeText.text.integerValue) {
+    }else if (_startTime == _endTime){
+        if (_startTime != _designationTime) {
             
             [alertError addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
             [self presentViewController:alertError animated:YES completion:nil];
@@ -92,26 +93,28 @@
 
 // 時刻確認
 - (Boolean)isTime {
+    
+    _startTime = _startTimeText.text.integerValue;
+    _endTime = _endTimeText.text.integerValue;
+    _designationTime = _designationTimeText.text.integerValue;
+
+    
     UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"時刻を入力してください。\n１時から24時までです。" message:nil preferredStyle:UIAlertControllerStyleAlert];
 
-    if ( _StartTimeText.text.integerValue < 1  || _StartTimeText.text.integerValue > 24) {
-        
+    if ( _startTime < 1  || _startTime > 24) {
         [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
         [self presentViewController:alert animated:YES completion:nil];
-
         
-        NSLog(@"NOOOO");
         return NO;
     }
-    if ( _EndTimeText.text.integerValue < 1  || _EndTimeText.text.integerValue > 24) {
+    if ( _endTime < 1  || _endTime > 24) {
         [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
         [self presentViewController:alert animated:YES completion:nil];
-
         
         return NO;
     }
     
-    if ( _DesignationTimeText.text.integerValue < 1  || _DesignationTimeText.text.integerValue > 24) {
+    if ( _designationTime < 1  || _designationTime > 24) {
         [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil]];
         [self presentViewController:alert animated:YES completion:nil];
 
